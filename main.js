8302c17c3f29c0ac8ee116151b0e0c4c9414c87b
Maken Hubbard <makhub26@outlook.com>
2018-03-06 17:12:12 -0600

    added challenge 4

--- main.js
+++ main.js
@@ -130,6 +130,21 @@ document.getElementById("challenge3B").innerHTML = '<h1>' + unique + '</h1>';
 
 
 
+//challenge 4
+var challengeFourInputA = [ 1, 2, 3 ];  // [2, 4, 6]
+var challengeFourInputB = [ 3, 8, 1, 2, 4, 12 ];  // [ 6, 16, 2, 4, 8, 24 ]
+
+for (var i = 0; i < challengeFourInputA.length; i++) {
+    challengeFourInputA[i] = challengeFourInputA[i]*2;
+}
+for (var i = 0; i < challengeFourInputB.length; i++) {
+    challengeFourInputB[i] = challengeFourInputB[i]*2;
+}
+
+document.getElementById('challenge4A').innerHTML = '<h1>' + challengeFourInputA.join(',') + '</h1>';
+document.getElementById('challenge4B').innerHTML = '<h1>' + challengeFourInputB.join(','); + '</h1>';
+
+
 
 
 
